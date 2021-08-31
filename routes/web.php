774be4ca9b763608d2bb5be7926b0dd2e838b2bb
Michihide Hotta <mhotta@ossbn.co.jp>
2021-08-31 11:39:20 +0900

    install laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/greeting', function () {
-    return 'Hello World';
-});
-Route::get('admin',function(){
-    return view('adminlte');
-});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
